790f24b1122ba8e668eeca6acfc4ee289fea527d
sunn4room <sunn4room@163.com>
2024-07-09 17:33:06 +0800

    change the html title

--- conf.py
+++ conf.py
@@ -10,8 +10,8 @@ master_doc = 'index'
 
 project = 'sunn4room'
 
-html_theme = "sphinx_book_theme"
-
+html_title = "sunn4room's docs"
+html_theme = 'sphinx_book_theme'
 html_theme_options = {
     'repository_url': 'https://github.com/sunn4room/docs',
     'use_repository_button': True,
